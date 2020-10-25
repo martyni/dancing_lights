212971012c1419ee05f997c87ebec6119cc47f62
Martyn Pratt <m@a>
2020-10-25 23:11:24 +0000

    formated reset text

--- app.py
+++ app.py
@@ -3,10 +3,24 @@ import subprocess
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def restarr():
     cmd = "echo 'sudo systemctl restart dl' | at now + 1 minutes"
+    page = '<pre style="font-size: 45px"> {} </pre>'
+    return page.format(
+            subprocess.check_output([
+        "bash", "-c", cmd],
+        stderr=subprocess.STDOUT
+        ).decode("utf-8")
+    )
+
+
+
+@app.route('/status')
+def status():
     return subprocess.check_output([
-       "bash", "-c", cmd])
+       "systemctl", "status", "dl"])
+
+
 
 if __name__ == '__main__':
     app.run( host='0.0.0.0')
